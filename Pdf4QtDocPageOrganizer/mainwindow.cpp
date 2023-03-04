93be40886ae5c3a42ec5ea5153527d819543f58e
Kask29 <98609356+Kask29@users.noreply.github.com>
2023-03-04 20:12:53 +0100

    Allow to drop file from system explorer

--- Pdf4QtDocPageOrganizer/mainwindow.cpp
+++ Pdf4QtDocPageOrganizer/mainwindow.cpp
@@ -14,7 +14,7 @@
 //
 //    You should have received a copy of the GNU Lesser General Public License
 //    along with PDF4QT.  If not, see <https://www.gnu.org/licenses/>.
-
+ 
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
